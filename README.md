520dade0ee5e5bc59f8ac3a50be5bca998d0aea4
Jeroen Claassens <support@favware.tech>
2023-12-09 00:17:01 +0100

    feat: add action code

---
+++ README.md
@@ -0,0 +1,37 @@
+# SSH Remote Action
+
+SSH Remote Action (ssh-remote-action) is a GitHub action for executing a SSH command on a remote server.
+An example use case of this action is deploying your application on your remote server after publishing a new release through another GitHub workflow job.
+
+## Usage
+
+`.github/workflows/continuous-deployment.yml`
+
+```yml
+on:
+  release:
+    types: [published]
+
+jobs:
+  publish:
+    name: Publish application
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout Project
+        uses: actions/checkout@v4
+      # Do whatever you need to do to publish your application
+
+  deploy:
+    name: Deploy on remote server
+    runs-on: ubuntu-latest
+    needs: publish
+    steps:
+      - name: Deploy on remote server
+        uses: favware/ssh-remote-action@v1
+        with:
+          host: ${{ secrets.SSH_HOST }}
+          port: ${{ secrets.SSH_PORT }}
+          key: ${{ secrets.SSH_KEY }}
+          username: ${{ secrets.SSH_USERNAME }}
+          command: ${{ secrets.SSH_COMMAND }}
+```
---
+++ ssh-remote-action.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Check if SSH_USERNAME is set
+if [ -z "${SSH_USERNAME}" ]; then
+    echo "with.username is not set. Exiting..."
+    exit 1;
+fi
+
+# Check if SSH_HOST is set
+if [ -z "${SSH_HOST}" ]; then
+    echo "with.host is not set. Exiting..."
+    exit 1;
+fi
+
+# Check if SSH_COMMAND is set
+if [ -z "${SSH_COMMAND}" ]; then
+    echo "with.command is not set. Exiting..."
+    exit 1;
+fi
+
+# Default to SSH port 22
+SSH_PORT=${SSH_PORT:-22}
+
+# Define the default SSH arguments that apply for both key and password flows
+SSH_ARGS="-p ${SSH_PORT} ${SSH_USERNAME}@${SSH_HOST} \"${SSH_COMMAND}\""
+
+eval "$(ssh-agent -s)"
+
+if [ -n "${SSH_KEY}" ]; then
+
+    # Write the private key to a file
+    echo "${SSH_KEY}" > ./private.key
+
+    # Set the permissions
+    chmod 600 ./private.key
+
+    # Check if the SSH_KEY_PASSPHRASE is empty
+    if [ -z "${SSH_KEY_PASSPHRASE}" ]; then
+        # Add the private key to the SSH agent
+        ssh-add ./private.key < /dev/null
+    else
+        # Create a script that will echo the passphrase
+        printf "%s\n" "#!/usr/bin/env bash" "echo ${SSH_KEY_PASSPHRASE}" > ./ssh-passphrase
+
+        # Set the permissions
+        DISPLAY=1 SSH_ASKPASS="./ssh-passphrase" ssh-add ./private.key < /dev/null
+    fi
+
+    # Execute the SSH command
+    ssh ${SSH_ARGS}
+
+elif [ -n "${SSH_PASSWORD}" ]; then
+
+    # Execute the SSH command
+    sshpass -p "${SSH_PASSWORD}" ssh ${SSH_ARGS}
+
+else
+    echo "Neither inputs.key nor inputs.password are set. Exiting..."
+    exit 1;
+fi
